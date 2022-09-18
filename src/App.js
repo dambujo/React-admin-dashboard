56e0c6f116e23c42a2ba9bafb0461aa45a532f28
Antonio Dambujo <pdambuja@gmail.com>
2022-09-18 20:32:52 +0200

    setup pages and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { Navbar, Sidebar, ThemeSettings, Footer } from './components';
+import {
+  Area,
+  Bar,
+  Calendar,
+  ColorMapping,
+  ColorPicker,
+  Customers,
+  Ecommerce,
+  Editor,
+  Employees,
+  Financial,
+  Kanban,
+  Line,
+  Orders,
+  Pie,
+  Pyramid,
+  Stacked,
+} from './pages';
 
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
@@ -22,7 +41,52 @@ const App = () => {
               </button>
             </TooltipComponent>
           </div>
-          {activeMenu ? <div>Sidebar</div> : <div>Sidebar w-0</div>}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dar:bg-secondary-dark-bg bg-white">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? 'md:ml-72' : 'flex-2'
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element="" />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+
+              {/* Pages */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calender" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* Cahrts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+            </Routes>
+          </div>
         </div>
       </BrowserRouter>
     </div>
---
+++ src/pages/Charts/ColoManager.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ColoManager = () => {
+  return <div>ColoManager</div>;
+};
+
+export default ColoManager;
